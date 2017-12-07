d7160f7e9cdbe2c8def58bc8e4e241936a17d809
shekhuverma <shekharvrm47@gmail.com>
2017-12-07 23:02:56 +0530

    Improved for calling from other script

--- CSV_reader.py
+++ CSV_reader.py
@@ -1,7 +1,16 @@
 import csv
 import time
 
-def bday_checker(dic,month,day):
+import pickle
+
+pickle_out = open("dic.pickle","wb")
+month=time.localtime(time.time())[1]   #current month acc to system
+day=time.localtime(time.time())[2]      #current day acc to system
+dic={} #To store month wise bdays
+
+file_name='b_days.csv'
+
+def bday_checker(dic):
     lis=[] #to store multiple bday on same date
     for a in dic:                                                      # To check bday on current date
         if int(a)==month:
@@ -9,22 +18,17 @@ def bday_checker(dic,month,day):
                 temp=int(x[1][0:2])
                 if temp==day:
                     lis.append(x[0])
-                    print "Bdaysssss"
     return lis
                 
-month=time.localtime(time.time())[1]   #current month acc to system
-day=time.localtime(time.time())[2]      #current day acc to system
-dic={} #To store month wise bdays
+def list_data(file_name):
+    with open(file_name) as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=',')
+        for row in readCSV:
+            print(row)
+            if dic.has_key(row[1][3:5]):
+                dic[row[1][3:5]].append(row)
+            else:
+                dic[row[1][3:5]]=[row]
+        pickle.dump(dic, pickle_out)
+        pickle_out.close()
 
-file_name='b_days.csv'
-
-with open(file_name) as csvfile:
-    readCSV = csv.reader(csvfile, delimiter=',')
-    for row in readCSV:
-        print(row)
-        if dic.has_key(row[1][3:5]):
-            dic[row[1][3:5]].append(row)
-        else:
-            dic[row[1][3:5]]=[row]
-            
-print bday_checker(dic,month,day)
